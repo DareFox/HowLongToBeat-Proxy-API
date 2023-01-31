2cfe5e94857de0226e8dc90bef35bd45a7b924e1
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-01 00:15:31 +0300

    Add router to query

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import express from 'express'
-import { queryGames } from "./howlongtobeat";
+import { queryGames } from "./hltb/query";
 
 const port = 1337
 const app = express()
 
-// app.get('/', (req, res) => {
-//     res.send("Sleepin' on a subway. City up above me. Dreamin' up the words to this song. Bettin' on a someday. Wakin' up at someplace. Believe me, baby, I know")
-// })
+app.get('/', async (req, res) => {
+    res.send("Sleepin' on a subway. City up above me. Dreamin' up the words to this song. Bettin' on a someday. Wakin' up at someplace. Believe me, baby, I know")
+})
 
+app.get('/api/query', async (req, res) => {
+    const title = req.query.title
+    if (title) {
+        res.json(await queryGames(title as string))
+    } else {
+        res.status(400).send({
+            message: "No title argument"
+        })
+    }
+})
 
-// app.listen(port, () => {
-//     console.log(`[server]: Server is running at http://localhost:${port}`);
-// });
-
-async function hehe() {
-    console.log(await queryGames("Age of Empires II"))
-}
-
-hehe()
-console.log('test')
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
