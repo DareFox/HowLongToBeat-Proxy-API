f1fc9588bf09a840b23596e854bcb478360e7edf
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-01 15:17:13 +0300

    Move from express to fastify

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,45 @@
-import express from 'express'
+import Fastify from "fastify";
 import { queryGames } from "./hltb/query";
 
-const port = 1337
-const app = express()
+const port = 1337;
+const app = Fastify({
+    logger: true,
+});
 
-app.get('/', async (req, res) => {
-    res.send("Sleepin' on a subway. City up above me. Dreamin' up the words to this song. Bettin' on a someday. Wakin' up at someplace. Believe me, baby, I know")
-})
+app.get("/", async (req, res) => {
+    res.send(
+        "Sleepin' on a subway. City up above me. Dreamin' up the words to this song. Bettin' on a someday. Wakin' up at someplace. Believe me, baby, I know"
+    );
+});
 
-app.get('/api/query', async (req, res) => {
-    const title = req.query.title
-    if (title) {
-        res.json(await queryGames(title as string))
-    } else {
-        res.status(400).send({
-            message: "No title argument"
-        })
+
+app.route<{
+    Querystring: {
+        title: string
+    },
+}>({
+    method: 'GET',
+    url: '/api/query',
+    handler: async (request, reply) => {
+        const title = request.query.title;
+        if (title) {
+            throw Error('test')
+            reply.send(await queryGames(title));
+        } else {
+            reply.status(400).send({
+                message: "No title argument",
+            });
+        }
     }
 })
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+const start = async () => {
+    try {
+        await app.listen({ port: port });
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
