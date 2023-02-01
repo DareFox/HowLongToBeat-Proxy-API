5eac0dd3c62fd969a70515f6ee1c9660985060a1
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-01 23:08:05 +0300

    Add coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+//    implementation("org.http4k:http4k-bom:4.37.0.0")
+//    implementation("org.http4k:http4k-core")
+//    implementation("org.http4k:http4k-server-undertow")
+//    implementation("org.http4k:http4k-client-apache")
 }
+
 
 tasks.test {
     useJUnitPlatform()
