8e8f3c45ed7999e107117f15b0274a7d2a0013a9
DareFox <easymalink@gmail.com>
2023-02-02 00:29:08 +0300

    http4k: Add routes

---
+++ src/main/kotlin/http4k/routes.kt
@@ -0,0 +1,28 @@
+package http4k
+
+import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.BAD_REQUEST
+import org.http4k.core.Status.Companion.OK
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+
+val serverRoutes = routes(
+    "/" bind GET to {
+        Response(OK).body(
+            "Sleepin' on a subway.\n" +
+            "City up above me. Dreamin' up the words to this song.\n" +
+            "Bettin' on a someday. Wakin' up at someplace.\n" +
+            "Believe me, baby, I know")
+    },
+    "/v1/query" bind GET to {
+        val title = it.query("title")
+
+        if (title != null) {
+            Response(OK).body("you GET bob")
+        } else {
+            ErrorResponse(BAD_REQUEST, "No title argument")
+        }
+    },
+)
+
