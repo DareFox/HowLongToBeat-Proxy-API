75c8abc64e591ae8e227d102ac59dd38dd019c53
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-15 03:57:41 +0300

    http4k: use kotlinx instead of jackson on error

--- src/main/kotlin/http4k/createErrorResponse.kt
+++ src/main/kotlin/http4k/createErrorResponse.kt
@@ -1,21 +1,21 @@
 package http4k
 
+import kotlinx.serialization.json.JsonObjectBuilder
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 import org.http4k.core.Body
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.core.with
-import org.http4k.format.Jackson
-import org.http4k.format.Jackson.json
+import org.http4k.format.KotlinxSerialization.json
 
+fun ErrorResponse(httpStatus: Status, message: String): Response {
+    val json = buildJsonObject {
+        put("code", httpStatus.code)
+        put("error", httpStatus.description)
+        put("message", message)
+    }
 
-val json = Jackson
-
-fun ErrorResponse(code: Status, message: String): Response {
-    return Response(code).with(
-        Body.json().toLens() of json.obj(
-            "code" to json.number(code.code),
-            "error" to json.string(code.description),
-            "message" to json.string(message)
-        )
-    )
+    return Response(httpStatus)
+        .with(Body.json().toLens() of json)
 }
