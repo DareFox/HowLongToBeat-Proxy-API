484a33154a5281e25f1a7cb614fbaa226cf9f4de
DareFox <easymalink@gmail.com>
2023-02-15 03:52:09 +0300

    gradle: remove coroutines and jackson, add kotlinx json

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ val http4kVersion = "4.37.0.0"
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
     application
 }
 
@@ -16,16 +17,17 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
     implementation("org.http4k:http4k-core:$http4kVersion")
     implementation("org.http4k:http4k-server-jetty:$http4kVersion")
-    implementation("org.http4k:http4k-format-jackson:$http4kVersion")
     implementation("org.http4k:http4k-client-apache:$http4kVersion")
+    implementation("org.http4k:http4k-format-kotlinx-serialization:$http4kVersion")
 
     implementation("org.slf4j:slf4j-simple:2.0.6")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 }
 
 
