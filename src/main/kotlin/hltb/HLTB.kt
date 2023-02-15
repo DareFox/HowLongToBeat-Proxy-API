b1b7362d17499fe29b7f0ee440c7b49a9b1949d9
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-15 03:58:38 +0300

    project: auto cleanup (imports, tabs)

--- src/main/kotlin/hltb/HLTB.kt
+++ src/main/kotlin/hltb/HLTB.kt
@@ -3,7 +3,7 @@ package hltb
 import http4k.client
 import kotlinx.serialization.Serializable
 import org.http4k.core.Body
-import org.http4k.core.Method.*
+import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.format.KotlinxSerialization.auto
@@ -23,15 +23,17 @@ object HLTB {
         val requestObj = createRequestObj(title, page)
         val requestLens = Body.auto<HltbQueryRequest>().toLens()
 
-        val requestWithJson = requestLens(requestObj, Request(POST, "https://howlongtobeat.com/api/search").headers(
-            listOf(
-                "Authority" to "howlongtobeat.com",
-                "Content-Type" to "application/json",
-                "Origin" to "https://howlongtobeat.com",
-                "Referer" to "https://howlongtobeat.com/?q=$encoded",
-                "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+        val requestWithJson = requestLens(
+            requestObj, Request(POST, "https://howlongtobeat.com/api/search").headers(
+                listOf(
+                    "Authority" to "howlongtobeat.com",
+                    "Content-Type" to "application/json",
+                    "Origin" to "https://howlongtobeat.com",
+                    "Referer" to "https://howlongtobeat.com/?q=$encoded",
+                    "User-Agent" to "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+                )
             )
-        ))
+        )
 
         return client(requestWithJson)
     }
