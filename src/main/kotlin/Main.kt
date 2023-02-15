6741295ea41c54e1d602530e79f56591dfece13c
DareFox <easymalink@gmail.com>
2023-02-15 03:53:25 +0300

    proxy: change port to 1337

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,17 @@
 import cache.WeakExpiringLRUCache
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.routing.bind
+import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import proxy.queryGames
+import proxy.serverRoutes
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
-import http4k.serverRoutes
+//import proxy.getServerRoutes
 
 val cache = WeakExpiringLRUCache<String, String>(10_000, 1.toDuration(DurationUnit.HOURS))
-
 fun main(args: Array<String>) {
-    serverRoutes.asServer(Jetty(1338)).start()
-    readln()
+    serverRoutes.asServer(Jetty(1337))
 }
