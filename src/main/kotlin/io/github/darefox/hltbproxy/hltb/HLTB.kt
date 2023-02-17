6ae9148a337170f1dc6e2efe6ebd44cfe6aec133
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-17 17:20:08 +0300

    hltb: simplify query function

--- src/main/kotlin/io/github/darefox/hltbproxy/hltb/HLTB.kt
+++ src/main/kotlin/io/github/darefox/hltbproxy/hltb/HLTB.kt
@@ -12,30 +12,14 @@ import java.net.URLEncoder
 
 object HLTB {
     fun queryGames(title: String, page: Int): HltbQueryResponse {
-        val response = requestSearch(title, page)
-        val lens = Body.auto<HltbQueryResponse>().toLens()
-
-        return lens(response)
-    }
-
-    fun getInfoAboutGame(id: Long) {
-
-    }
-
-    private fun requestSearch(title: String, page: Int): Response {
         val encoded = URLEncoder.encode(title, "utf-8")
         val url = "https://howlongtobeat.com/?q=$encoded"
 
         val queryObj = createQueryObj(title, page)
-        val requestLens = Body.auto<HltbQueryRequest>().toLens()
-
-        return client(
+        val response =  client(
             Request(POST, "https://howlongtobeat.com/api/search").hltbJsonRequest(url, queryObj)
         )
-    }
-}
 
-@Serializable
-private data class SearchResponse(
-    val data: List<QueryGamesResponse>
-)
+        return Body.auto<HltbQueryResponse>().toLens().invoke(response)
+    }
+
