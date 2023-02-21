da6d6e1036e8e8d1cd08ff8651d23095d8d9b8d2
DareFox <easymalink@gmail.com>
2023-02-21 03:19:05 +0300

    hltb: add speedrun data class parser

---
+++ src/main/kotlin/io/github/darefox/hltbproxy/hltb/HltbSpeedrunTable.kt
@@ -0,0 +1,31 @@
+package io.github.darefox.hltbproxy.hltb
+
+data class HltbSpeedrunTable(
+    val anyPercentage: HltbSpeedrunTime,
+    val hundredPercentage: HltbSpeedrunTime,
+)
+
+data class HltbSpeedrunTime(
+    val averageSec: Long?,
+    val medianSec: Long?,
+    val leastSec: Long?,
+    val mostSec: Long?
+)
+
+fun HltbTableParser.toSpeedrun(): HltbSpeedrunTable {
+    require(title == "Speedruns")
+
+    return HltbSpeedrunTable(
+        anyPercentage = getVariants(rows["Any%"]!!),
+        hundredPercentage = getVariants(rows["100%"]!!),
+    )
+}
+
+private fun HltbTableParser.getVariants(column: Map<String,String>): HltbSpeedrunTime {
+    return HltbSpeedrunTime(
+        averageSec = column["Average"]?.let { toSecondsOrNull(it) },
+        medianSec = column["Median"]?.let { toSecondsOrNull(it) },
+        leastSec = column["Fastest"]?.let { toSecondsOrNull(it) },
+        mostSec = column["Slowest"]?.let { toSecondsOrNull(it) },
+    )
+}
