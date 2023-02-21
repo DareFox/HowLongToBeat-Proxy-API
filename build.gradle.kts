577098a12c24cdb9bdedb677ac4ed66d69c5a94e
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-22 01:50:11 +0300

    gradle: use jsoup 1.15.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,18 +19,24 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
+    // http4k
     implementation("org.http4k:http4k-core:$http4kVersion")
     implementation("org.http4k:http4k-server-jetty:$http4kVersion")
     implementation("org.http4k:http4k-client-apache:$http4kVersion")
     implementation("org.http4k:http4k-format-kotlinx-serialization:$http4kVersion")
 
+    // loggers
     implementation("org.slf4j:slf4j-simple:2.0.6")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
 
+    // serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
+    // dependency for mutex
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
-    implementation("org.jsoup:jsoup:1.15.3")
+    // html parser
+    implementation("org.jsoup:jsoup:1.15.4")
 }
 
 
