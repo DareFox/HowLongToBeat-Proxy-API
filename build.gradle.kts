5b97336c1b479e007ef3a95c2cb35595db04c1c7
DareFox <easymalink@gmail.com>
2023-02-21 03:51:12 +0300

    gradle: add delecious jsoup

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    implementation("org.jsoup:jsoup:1.15.3")
 }
 
 
