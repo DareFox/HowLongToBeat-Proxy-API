ea18ed18bf45186425eb37c6769339a42d58dd2e
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-22 03:21:58 +0300

    proxy: set default port to 8080

--- src/main/kotlin/io/github/darefox/hltbproxy/Main.kt
+++ src/main/kotlin/io/github/darefox/hltbproxy/Main.kt
@@ -6,6 +6,6 @@ import org.http4k.server.asServer
 
 
 fun main(args: Array<String>) {
-    val port = System.getenv("PORT")?.toIntOrNull() ?: 443
+    val port = System.getenv("PORT")?.toIntOrNull() ?: 8080
     serverRoutes.asServer(Jetty(port)).start()
 }
