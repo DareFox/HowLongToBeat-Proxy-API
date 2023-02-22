627af2c7e03787f5c46c72d9d1e714363424aaa2
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-22 03:39:39 +0300

    hltb: make rows in table nullable (execpt "all styles")

--- src/main/kotlin/io/github/darefox/hltbproxy/hltb/HltbSpeedrunTable.kt
+++ src/main/kotlin/io/github/darefox/hltbproxy/hltb/HltbSpeedrunTable.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class HltbSpeedrunTable(
-    val anyPercentage: HltbSpeedrunTime,
-    val hundredPercentage: HltbSpeedrunTime,
+    val anyPercentage: HltbSpeedrunTime?,
+    val hundredPercentage: HltbSpeedrunTime?,
 )
 
 @Serializable
