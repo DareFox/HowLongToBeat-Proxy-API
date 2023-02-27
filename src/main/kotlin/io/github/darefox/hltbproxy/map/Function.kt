d3ad6cc4ba23c005f58a2c306f58a41931653ee6
DareFox <easymalink@gmail.com>
2023-02-27 09:21:44 +0300

    map+gradle: replace DIY map with Apache

--- src/main/kotlin/io/github/darefox/hltbproxy/map/Function.kt
+++
@@ -1,3 +0,0 @@
-package io.github.darefox.hltbproxy.map
-
-fun <K,V> collectableMutableMap(): MutableMap<K,V> = CollectableHashMap<K,V>()
--- src/main/kotlin/io/github/darefox/hltbproxy/map/KeyValuePair.kt
+++
@@ -1,7 +0,0 @@
-package io.github.darefox.hltbproxy.map
-
-internal data class KeyValuePair<K,V>(
-    val hash: Int,
-    val key: K,
-    var value: V,
-)
