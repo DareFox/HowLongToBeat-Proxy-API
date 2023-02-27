967e1e7aa11c82358cce821ba4a4735ae7a53dbb
DareFox <47672780+DareFox@users.noreply.github.com>
2023-02-27 17:44:51 +0300

    proxy: send json about cache info

--- src/main/kotlin/io/github/darefox/hltbproxy/proxy/CacheInfo.kt
+++ src/main/kotlin/io/github/darefox/hltbproxy/proxy/CacheInfo.kt
@@ -1,9 +1,19 @@
 package io.github.darefox.hltbproxy.proxy
 
-import org.http4k.core.HttpHandler
-import org.http4k.core.Response
-import org.http4k.core.Status
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.buildJsonObject
+import org.http4k.core.*
+import org.http4k.format.KotlinxSerialization.auto
 
 val cacheInfo: HttpHandler = {
-   Response(Status.OK).body(cache.size.toString())
+    val json = buildJsonObject {
+        put("lifetime", JsonPrimitive(cache.lifetime.toString()))
+        put("size", JsonPrimitive(cache.size))
+        put("maxSize", JsonPrimitive(cache.maxSize))
+    }
+
+   Response(Status.OK).with(
+       Body.auto<JsonObject>().toLens() of json
+   )
 }
