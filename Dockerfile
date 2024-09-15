9b5f3d4106f5c9e18cb423ad536dfd48a95bf42d
DareFox <47672780+DareFox@users.noreply.github.com>
2024-09-15 16:27:09 +0300

    chore: Update gradle, kotlin and dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM gradle:8.0.1-jdk17 AS build
+FROM gradle:8.10.1-jdk21 AS build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle shadowJar --no-daemon
 
-FROM eclipse-temurin:17-jre-alpine
+FROM eclipse-temurin:21-jre-alpine
 
 COPY --from=build /home/gradle/src/build/libs/ /app/
 ENTRYPOINT ["java","-jar","/app/HowLongToBeat-Proxy-API-1.0-SNAPSHOT-all.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 val coroutinesVersion = "1.6.4"
-val http4kVersion = "4.37.0.0"
+val http4kVersion = "5.26.0.0"
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.0"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     application
 }
@@ -26,28 +26,24 @@ dependencies {
     implementation("org.http4k:http4k-format-kotlinx-serialization:$http4kVersion")
 
     // loggers
-    implementation("org.slf4j:slf4j-simple:2.0.6")
-    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 
     // serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 
     // dependency for mutex
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 
     // html parser
-    implementation("org.jsoup:jsoup:1.15.4")
+    implementation("org.jsoup:jsoup:1.18.1")
 
-    implementation("org.apache.commons:commons-collections4:4.1")
+    implementation("org.apache.commons:commons-collections4:4.4")
 }
 
 
 tasks.test {
     useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 tasks.jar {
